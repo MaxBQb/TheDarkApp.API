9f0e078e15f0ffc519ab0e00524d6c9ad76cb9c9
Бабышев Максим <cmdazaza@gmail.com>
2021-11-21 16:40:29 +0300

    Init

---
+++ src/main/kotlin/lab/maxb/dark_api/DarkApiApplication.kt
@@ -0,0 +1,11 @@
+package lab.maxb.dark_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DarkApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DarkApiApplication>(*args)
+}
