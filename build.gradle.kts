99da6137acc95b9dadcf69683685da23f0514796
Бабышев Максим <cmdazaza@gmail.com>
2022-05-02 11:55:53 +0300

    Fix: JPA/Postgres lib versions incompatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,17 +18,17 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.13")
-    implementation("org.springdoc:springdoc-openapi-security:1.5.13")
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.13")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+    implementation("org.springdoc:springdoc-openapi-security:1.6.7")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("javax.xml.bind:jaxb-api")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.5.6")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.6")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.7")
+    runtimeOnly("org.postgresql:postgresql:42.3.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.7")
 }
 
 tasks.withType<KotlinCompile> {
