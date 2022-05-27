4e41ec13037c8e0a7093556a22bb155f1db91dbb
Бабышев Максим <cmdazaza@gmail.com>
2022-05-27 22:46:31 +0300

    + First Unit-Test

--- src/test/kotlin/lab/maxb/dark_api/DarkApiApplicationTests.kt
+++ src/test/kotlin/lab/maxb/dark_api/DarkApiApplicationTests.kt
@@ -1,13 +1,26 @@
 package lab.maxb.dark_api
 
+import lab.maxb.dark_api.model.UserCredentials
+import lab.maxb.dark_api.model.pojo.AuthRequest
+import lab.maxb.dark_api.repository.DatabaseFiller
+import lab.maxb.dark_api.services.AuthService
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DarkApiApplicationTests {
+class DarkApiApplicationTests @Autowired constructor(
+    private val authService: AuthService,
+    private val fillerConfig: DatabaseFiller.Properties,
+) {
 
     @Test
-    fun contextLoads() {
+    fun hasAdmin() {
+        assertEquals(authService.login(AuthRequest(
+            fillerConfig.admin.login,
+            fillerConfig.admin.password,
+        ))?.role, UserCredentials.Role.ADMINISTRATOR)
     }
 
 }
